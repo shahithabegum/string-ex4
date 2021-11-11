5e72315538fd5d380db61e2fac1cb2c5cb9f705f
shahithabegum <69446892+shahithabegum@users.noreply.github.com>
2021-11-11 15:41:43 +0530

    Add files via upload

---
+++ se4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+class se22
+{
+  public static void main(String arg[])
+   {
+     try
+         {
+            String s ="Stephen Edwin King";
+            String s1 ="Walter Winchell";
+	
+	    System.out.println(s.equals(s1));
+	  
+	}
+	catch(Exception e)
+	{
+	  System.out.println(e);
+	}
+       }
+}
